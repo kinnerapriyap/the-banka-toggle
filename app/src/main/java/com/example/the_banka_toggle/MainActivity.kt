1d5d4be3c417ff3da03971db229f75db8b53a540
kinnerapriyap <kinnerapriyap@gmail.com>
2021-02-08 13:15:01 +0900

    Add view variables to MainActivity

--- app/src/main/java/com/example/the_banka_toggle/MainActivity.kt
+++ app/src/main/java/com/example/the_banka_toggle/MainActivity.kt
@@ -2,8 +2,17 @@ package com.example.the_banka_toggle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    private val root: ConstraintLayout
+        get() = findViewById(R.id.root)
+
+    private val stretchableSquare: StretchableSquare
+        get() = findViewById(R.id.stretchable_square)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
