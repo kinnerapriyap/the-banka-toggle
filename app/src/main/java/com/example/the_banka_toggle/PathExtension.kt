e930e9d2732dc4d6d90d38c56fbb206b13459506
kinnerapriyap <kinnerapriyap@gmail.com>
2021-02-08 13:13:20 +0900

    Add extension functions for Path with PointF

---
+++ app/src/main/java/com/example/the_banka_toggle/PathExtension.kt
@@ -0,0 +1,28 @@
+package com.example.the_banka_toggle
+
+import android.graphics.Path
+import android.graphics.PointF
+
+fun Path.moveTo(point: PointF) {
+    moveTo(point.x, point.y)
+}
+
+fun Path.lineTo(point: PointF) {
+    lineTo(point.x, point.y)
+}
+
+fun Path.quadTo(controlPoint: PointF, endPoint: PointF) {
+    quadTo(controlPoint.x, controlPoint.y, endPoint.x, endPoint.y)
+}
+
+fun Path.addPoint(centerPoint: PointF) {
+    addCircle(centerPoint, 5f)
+}
+
+fun Path.addCircle(
+    centerPoint: PointF,
+    radius: Float,
+    direction: Path.Direction = Path.Direction.CW
+) {
+    addCircle(centerPoint.x, centerPoint.y, radius, direction)
+}
