28625778f925f0dd6740b117b408dc8c88a89457
kinnerapriyap <kinnerapriyap@gmail.com>
2021-02-10 18:01:38 +0900

    Remove unnecessary getters for bounds

--- app/src/main/java/com/example/the_banka_toggle/StretchableSquareBounds.kt
+++ app/src/main/java/com/example/the_banka_toggle/StretchableSquareBounds.kt
@@ -9,46 +9,37 @@ data class StretchableSquareBounds(
     val endPositionY: Float = 0f,
     val stretchFactor: Float = 1f
 ) {
-    private val incrementValueX
-        get() = (endPositionX - startPositionX) / 10
-    private val incrementValueY
-        get() = (endPositionY - startPositionY) / 10
-    private val midLeftPointX
-        get() = startPositionX + 4 * incrementValueX
-    private val midRightPointX
-        get() = startPositionX + 6 * incrementValueX
-    private val controlPointY
-        get() = startPositionY + 1 * incrementValueY
+    private val incrementValueX = (endPositionX - startPositionX) / 10
+    private val incrementValueY = (endPositionY - startPositionY) / 10
+    private val midLeftPointX = startPositionX + 4 * incrementValueX
+    private val midRightPointX = startPositionX + 6 * incrementValueX
+    private val controlPointY = startPositionY + 1 * incrementValueY
 
-    val midPositionX
-        get() = (startPositionX + endPositionX) / 2
-    val midPositionY
-        get() = (startPositionY + endPositionY) / 2
+    val midPositionX = (startPositionX + endPositionX) / 2
+    val midPositionY = (startPositionY + endPositionY) / 2
 
-    val topLeadingPoint: PointF
-        get() = PointF(startPositionX, startPositionY)
-    val topTrailingPoint: PointF
-        get() = PointF(endPositionX, startPositionY)
-    val controlPointToBottomTrailing: PointF
-        get() = getPointFByFactor(
+    val topLeadingPoint: PointF = PointF(startPositionX, startPositionY)
+    val topTrailingPoint: PointF = PointF(endPositionX, startPositionY)
+    val controlPointToBottomTrailing: PointF =
+        getPointFByFactor(
             PointF(endPositionX, controlPointY),
             PointF(midRightPointX, controlPointY),
             stretchFactor
         )
-    val bottomTrailingPoint: PointF
-        get() = getPointFByFactor(
+    val bottomTrailingPoint: PointF =
+        getPointFByFactor(
             PointF(endPositionX, endPositionY),
             PointF(midRightPointX, endPositionY),
             stretchFactor
         )
-    val bottomLeadingPoint: PointF
-        get() = getPointFByFactor(
+    val bottomLeadingPoint: PointF =
+        getPointFByFactor(
             PointF(startPositionX, endPositionY),
             PointF(midLeftPointX, endPositionY),
             stretchFactor
         )
-    val controlPointToTopLeading: PointF
-        get() = getPointFByFactor(
+    val controlPointToTopLeading: PointF =
+        getPointFByFactor(
             PointF(startPositionX, controlPointY),
             PointF(midLeftPointX, controlPointY),
             stretchFactor
